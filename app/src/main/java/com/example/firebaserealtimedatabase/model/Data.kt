5df1ec9feb35358965a4337a3389f4b89fa268d1
naeemnoman <naeemnoman0419@gmail.com>
2024-10-01 12:16:14 +0600

    done

---
+++ app/src/main/java/com/example/firebaserealtimedatabase/model/Data.kt
@@ -0,0 +1,11 @@
+package com.example.firebaserealtimedatabase.model
+
+data class Data(
+
+    var id:String? = null,
+    var name:String? = null,
+    var email:String? = null,
+    var subject:String? = null,
+    var birthDate:String? = null
+
+)
